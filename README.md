b93b18a87bc07a42180bfc288854c39ad9c0a076
jasonyou1995 <jason.you1995@gmail.com>
2018-12-12 20:32:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Hierarchical Deterministic Wallet For Ether
---
+++ hd-wallet_test2.go
@@ -0,0 +1,18 @@
+package hdwallet
+
+import (
+    "fmt"
+    "math/big"
+    "strings"
+    "testing"
+
+    "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// Table test
+func TestWallet(t *testing.T) {
+
+}
